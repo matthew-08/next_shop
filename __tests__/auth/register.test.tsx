ae6ec687057a4e651150e1eface5f37db4caed3f
Matthew <matthewjcrosby1@gmail.com>
2023-04-13 14:55:52 +0700

    unit tests for password fields

--- __tests__/auth/register.test.tsx
+++ __tests__/auth/register.test.tsx
@@ -10,6 +10,7 @@ jest.mock('next/router', () => require('next-router-mock'))
 const inputs = {
   email: () => screen.getByLabelText(/email/i),
   password: () => screen.getByLabelText('Password'),
+  confirmPassword: () => screen.getByLabelText('Confirm Password'),
 }
 
 const buttons = {
@@ -25,14 +26,14 @@ describe('Register Component', () => {
     )
   })
 
+  beforeEach(() => {
+    render(
+      <MockedProvider>
+        <Register />
+      </MockedProvider>
+    )
+  })
   describe('Error fields on empty input', () => {
-    beforeEach(() => {
-      render(
-        <MockedProvider>
-          <Register />
-        </MockedProvider>
-      )
-    })
     test('shows error message for email input', async () => {
       await userEvent.type(inputs.password(), '3333')
       await userEvent.click(buttons.submitButton())
@@ -42,6 +43,30 @@ describe('Register Component', () => {
       await userEvent.type(inputs.email(), 'auser@gmail.com')
       await userEvent.click(buttons.submitButton())
       expect(inputs.password()).toBeInvalid()
+    })
+  })
+  describe('email input messages', () => {
+    test('shows invalid email message if user does not adhere to email format', async () => {
+      await userEvent.type(inputs.email(), 'auser')
+      await userEvent.click(buttons.submitButton())
+      expect(await screen.findByText(/valid email/i)).toBeInTheDocument()
+      expect(inputs.email()).toBeInvalid()
+    })
+  })
+  describe('password input messages', () => {
+    test('shows error message when password is too short', async () => {
+      await userEvent.type(inputs.password(), '11')
+      await userEvent.click(buttons.submitButton())
+      expect(
+        await screen.findByText(/at least 6 characters/i)
+      ).toBeInTheDocument()
+    })
+    test('shows error message when passwords do not match', async () => {
+      await userEvent.type(inputs.password(), 'password123')
+      await userEvent.type(inputs.confirmPassword(), 'password1234')
+      await userEvent.click(buttons.submitButton())
+      expect(inputs.confirmPassword()).toBeInvalid()
+      expect(await screen.findByText(/match/)).toBeInTheDocument()
     })
   })
 })
