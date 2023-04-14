0bb8adc420cb504584661e4d4af80c57c42b657b
Matthew <matthewjcrosby1@gmail.com>
2023-04-14 10:08:27 +0700

    Created rough draft for a mock generator inside of __tests__/auth_mocks.ts

--- __tests__/auth/mocks.ts
+++ __tests__/auth/mocks.ts
@@ -1,6 +1,8 @@
 import { ApolloCache } from "@apollo/client";
 import * as Apollo from '@apollo/client'
-import { LogInMutationResult , LogInMutationVariables , LogInDocument, LoginType } from "graphql/generated/graphql";
+import { LogInMutationResult , LogInMutationVariables , LogInDocument, LoginType, LogInMutation, RegisterMutation, RegisterDocument, RegisterMutationVariables } from "graphql/generated/graphql";
+import Register from "@/pages/auth/register";
+
 
 
 const mockFactory = (query: Apollo.DocumentNode, mockResult) => (
@@ -37,8 +39,116 @@ const testMutation:MockMutation<typeof LogInDocument, LoginType, LogInMutationRe
   result: {
     data: {
       data: {
-        __typename
+        login: {
+          data: {
+            id: '12',
+            email: '123',
+            token: '123',
+            cart: {
+              id,
+
+            }
+          }
+        }
       }
     }
   }
 }
+
+type MockMutationTwo<T> = {
+  mutation: T
+}
+
+const testType:MockMutationTwo<LogInMutation> = {
+  mutation: {
+    login: {
+      
+    }
+  }
+}
+
+const mock = [
+  {
+    request: {
+      query: LogInDocument,
+      variables: {
+        UserRegisterInput: {
+          email: 'anemail@gmail.com',
+          password: 'password123',
+        },
+      },
+    },
+    result: {
+      data: {
+        register: {
+          __typename: 'MutationRegisterSuccess',
+          data: {
+            token: '123',
+            email: 'anemail@gmail.com',
+            id: '1',
+          },
+        },
+      },
+    },
+  },
+]
+
+type TestTest<QueryType extends Apollo.MutationResult> = {
+  result: {
+  }
+}
+
+
+
+type TestType = Ok<RegisterMutation>
+
+const est: TestType = {
+  data: {
+    register: {
+      __typename: 'MutationRegisterSuccess',
+      data: {
+        email: '1213',
+        id: '123123',
+        token: '123123'
+      }
+    }
+  }
+}
+
+type ReturnT<T, B> = {
+  request: {
+    query: Apollo.DocumentNode,
+    variables: B
+  }
+  result: {
+    data: T
+  }
+}
+
+function GenerateMock<T, B>(obj: T, queryType: Apollo.DocumentNode, variables: B ): ReturnT<T, B>{
+  return {
+    request: {
+      query: queryType,
+      variables,
+    },
+    result: {
+      data: obj,
+    }
+  }
+}
+
+const generatedMock = GenerateMock<RegisterMutation, RegisterMutationVariables>({
+  register: {
+    __typename: 'MutationRegisterSuccess',
+    data: {
+      email: '123@Gmail.com',
+      id: '123123',
+      token: '123123',
+    },
+  },
+}, RegisterDocument, {
+  UserRegisterInput: {
+    email: '123@Gmail.com',
+    password: '1123123'
+  }
+})
