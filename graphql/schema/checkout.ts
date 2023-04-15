8266d53fad6e97d5e778f2b5d5b54130ce9f2757
Matthew <matthewjcrosby1@gmail.com>
2023-04-15 11:46:18 +0700

    prisma query for retrieving shop items based on a cart id

--- graphql/schema/checkout.ts
+++ graphql/schema/checkout.ts
@@ -2,17 +2,35 @@ import prisma from 'prisma/db';
 import Stripe from 'stripe';
 import builder from '../builder';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {})
 
 
 builder.mutationFields((t) => ({
     checkout: t.field({
         args: {
-            input: t.arg({required: true, type: 'String'})
+            cartId: t.arg({required: true, type: 'String'})
         },
         type: 'String',
         resolve: (parent, args) => {
-            aait    
+            const cartItems = prisma.userCart.findUnique({
+                where: {
+                    id: Number(args.cartId)
+                },
+                include: {
+                    CartItem: {
+                        include: {
+                            item: {
+                                
+                            }
+                        }
+                    }
+                }
+            })
+            const session = stripe.checkout.sessions.create({
+                line_items: [
+
+                ]
+            }) 
         }
     })
 }))
