bb165bb6e0fde40a84c5663819bf1f1a523284c4
Matthew <matthewjcrosby1@gmail.com>
2023-04-19 23:41:05 +0700

    Schema debugging

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -13,7 +13,19 @@ export default createYoga<{
   req: NextApiRequest
   res: NextApiResponse
 }>({
-  schema,
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        greetings: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        greetings: () =>
+          'This is the `greetings` field of the root `Query` type',
+      },
+    },
+  }),
   // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
   graphqlEndpoint: '/api/graphql',
 })
