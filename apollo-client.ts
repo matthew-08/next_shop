2a4d401459fc743156af08564b944e35318bebce
Matthew <matthewjcrosby1@gmail.com>
2023-04-20 09:52:09 +0700

    Cleanup

--- apollo-client.ts
+++ apollo-client.ts
@@ -7,7 +7,7 @@ const httpLink = createHttpLink({
 })
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
+  // get the authentication token from local storage if it exist
   const token = getToken()
   // return the headers to the context so httpLink can read them
   return {
